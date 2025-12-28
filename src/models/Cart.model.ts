9b79d93cebbc7fc873ae2598343ff7c2569dd4fb
Ayaan <sharingan0384221@gmail.com>
2025-12-28 11:59:19 +0600

    fixed spacing in Cart model

--- src/models/Cart.model.ts
+++ src/models/Cart.model.ts
@@ -1,10 +1,31 @@
 import mongoose from "mongoose";
+import { Product } from "./Product.model";
 
-const CartSchema = new mongoose.Schema({
-  userId: {
+const CartItemSchema = new mongoose.Schema({
+  product: {
     type: mongoose.Schema.Types.ObjectId,
+    ref: Product,
+    required: true,
+  },
+  size: {
+    type: String,
+    required: true,
+  },
+  color: {
+    type: String,
     required: true,
   },
 });
+
+const CartSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: mongoose.Schema.Types.ObjectId,
+      required: true,
+    },
+    Items: [CartItemSchema],
+  },
+  { timestamps: true }
+);
 
 export const Cart = mongoose.model("Cart", CartSchema);
