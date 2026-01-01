63a6aecf5aa5e9e861b2917835575135109a30e2
Ayaan <sharingan0384221@gmail.com>
2026-01-01 15:54:17 +0600

    created the generate access token and refresh token method in user model file

--- src/models/User.model.ts
+++ src/models/User.model.ts
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import bcrypt from "bcrypt";
-
+import jwt from "jsonwebtoken";
+import { SignOptions } from "jsonwebtoken";
 const UserSchema = new mongoose.Schema(
   {
     fullName: {
@@ -42,9 +43,29 @@ UserSchema.pre("save", async function () {
   if (!this.isModified("password")) return;
   this.password = await bcrypt.hash(this.password, 10);
 });
+
 //validating pass
 UserSchema.methods.isPasswordCorrect = async function (password: string) {
   return await bcrypt.compare(password, this.password);
+};
+
+UserSchema.methods.generateAccessToken = async function () {
+  const payload = {
+    id: this._id,
+  };
+
+  return jwt.sign(payload, process.env.ACCESS_TOKEN_SECRET!, {
+    expiresIn: "15m",
+  });
+};
+
+UserSchema.methods.generateRefreshToken = function () {
+  const payload = {
+    id: this._id,
+  };
+  return jwt.sign(payload, process.env.REFRESH_TOKEN_SECRET!, {
+    expiresIn: "1d",
+  });
 };
 
 export const User = mongoose.model("User", UserSchema);
