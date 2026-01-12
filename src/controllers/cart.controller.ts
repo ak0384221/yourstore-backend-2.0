9c0e6c8a4e04d036e75c8f95aab6beb349692aeb
Ayaan <sharingan0384221@gmail.com>
2026-01-12 09:35:15 +0600

    flatted the cart model,added mongodb index,unique flag to per product,added new controller with new logic to make the cart easy crud operation

--- src/controllers/cart.controller.ts
+++ src/controllers/cart.controller.ts
@@ -1,67 +1,59 @@
-import { Cart, CartItemSchema } from "../models/Cart.model.ts";
+import { Cart } from "../models/Cart.model.ts";
 import { Product } from "../models/Product.model.ts";
 import { ApiError } from "../utils/apiError.ts";
 import { ApiResponse } from "../utils/apiResponse.ts";
 import { asyncHandler } from "../utils/asyncHandler.ts";
 
 const addToCart = asyncHandler(async (req, res) => {
-  //get id,size,color
+  //get id,size,color and the user
   //validate inputs
-  // 1.check the user is existed or not
-  // 2.if not then  - validate product and varients and create one and insert into the cart
-  // 3.if existed then check the array items and check if the product is existed wit the exact varients
-  // 4.if same then update the quntity
-  // 5.if not same then include one more item of the new product in the cart
-
-  //getting the fields
+  //check if that specfic user has already the specific product with same atrribute or not
+  //if same then update the quantity andif not then add another cart
+  //-----------------------//
   const { productId, selectedSize, selectedColor, selectedQuantity } = req.body;
-  //get the user
   const user = req.user;
-  //validate fields
   if (
     !productId ||
     !selectedSize ||
     !selectedColor ||
-    selectedQuantity == null
+    selectedQuantity == null ||
+    selectedQuantity == 0
   ) {
     throw new ApiError(400, "fields must not be empty");
   }
 
-  //-------------------------------------------------------------------------------//
-
-  //1.find the user from cart
-  const userExisted = await Cart.findOne({ userId: user._id });
-
-  //2.if not found create new cart
-  if (!userExisted) {
-    //check 1 => products availabilty
-    const productAvailable = await Product.findById(productId);
-    if (!productAvailable) {
+  //1.find the user cart from cart
+  const existedCartItem = await Cart.findOne({
+    userId: user._id,
+    product: productId,
+    selectedSize,
+    selectedColor,
+  });
+  if (!existedCartItem) {
+    const isProductAvailable = await Product.findById(productId);
+    if (!isProductAvailable) {
       throw new ApiError(400, "product unavailable");
     }
 
     //check 2 => varients availabilty
-    const variantAvailable = productAvailable.varients.find((v) => {
+    const isVariantAvailable = isProductAvailable.varients.find((v) => {
       return (
         v.size === selectedSize &&
         v.color === selectedColor &&
         v.stock >= selectedQuantity
       );
     });
-    if (!variantAvailable) {
+    if (!isVariantAvailable) {
       throw new ApiError(400, "product unavailable");
     }
+
     //create new cart item
     const newCartItem = await Cart.create({
       userId: user._id,
-      items: [
-        {
-          product: productId,
-          selectedColor,
-          selectedSize,
-          selectedQuantity,
-        },
-      ],
+      product: productId,
+      selectedColor,
+      selectedSize,
+      selectedQuantity,
     });
 
     return res
@@ -69,63 +61,11 @@ const addToCart = asyncHandler(async (req, res) => {
       .json(new ApiResponse(200, newCartItem, "cart item added succesfully"));
   }
 
-  //3.if user found/existed
-  //check if it is the same
-  if (userExisted) {
-    const existingItem = userExisted.items.find(
-      (item) =>
-        item.product.toString() === productId.toString() &&
-        item.selectedColor === selectedColor &&
-        item.selectedSize === selectedSize
-    );
-    if (existingItem) {
-      if (existingItem.selectedQuantity === Number(selectedQuantity)) {
-        // Exact same item already in cart → reject
-        throw new ApiError(500, "product already existed");
-      } else {
-        // Quantity is different → update quantity
-        existingItem.selectedQuantity = selectedQuantity;
-        await userExisted.save();
-
-        return res
-          .status(200)
-          .json(
-            new ApiResponse(200, "cart items quantity updated succesfully")
-          );
-      }
-    } else {
-      //check 1 => products availabilty
-      const productAvailable = await Product.findById(productId);
-      if (!productAvailable) {
-        throw new ApiError(400, "product unavailable");
-      }
-
-      //check 2 => varients availabilty
-      const variantAvailable = productAvailable.varients.find((v) => {
-        return (
-          v.size === selectedSize &&
-          v.color === selectedColor &&
-          v.stock >= selectedQuantity
-        );
-      });
-      if (!variantAvailable) {
-        throw new ApiError(400, "product unavailable");
-      }
-      //create new cart item
-      userExisted.items.push({
-        product: productId,
-        selectedColor,
-        selectedSize,
-        selectedQuantity,
-      });
-
-      const newCartItem = await userExisted.save();
-
-      return res
-        .status(200)
-        .json(new ApiResponse(200, newCartItem, "cart item added succesfully"));
-    }
-  }
+  existedCartItem.selectedQuantity += Number(selectedQuantity);
+  const updatedItem = await existedCartItem.save();
+  res
+    .status(200)
+    .json(new ApiResponse(200, updatedItem, "cart item added succesfully"));
 });
 
 export { addToCart };
--- src/models/Cart.model.ts
+++ src/models/Cart.model.ts
@@ -1,36 +1,37 @@
 import mongoose from "mongoose";
 import { Product } from "./Product.model.ts";
 
-const CartItemSchema = new mongoose.Schema({
-  product: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: Product,
-    required: true,
-  },
-  selectedSize: {
-    type: String,
-    required: true,
-  },
-  selectedColor: {
-    type: String,
-    required: true,
-  },
-  selectedQuantity: {
-    type: Number,
-    required: true,
-    min: 1,
-  },
-});
-
 const CartSchema = new mongoose.Schema(
   {
     userId: {
       type: mongoose.Schema.Types.ObjectId,
       required: true,
     },
-    items: [CartItemSchema],
+    product: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: Product,
+      required: true,
+    },
+    selectedSize: {
+      type: String,
+      required: true,
+    },
+    selectedColor: {
+      type: String,
+      required: true,
+    },
+    selectedQuantity: {
+      type: Number,
+      required: true,
+      min: 1,
+    },
   },
   { timestamps: true }
+);
+
+CartSchema.index(
+  { user: 1, product: 1, selectedSize: 1, selectedColor: 1 },
+  { unique: true }
 );
 
 export const Cart = mongoose.model("Cart", CartSchema);
